029a1310408297452c65141128015660253f4237
dattruong88vn <dattt1.ho@mcredit.com.vn>
2023-10-23 00:03:06 +0700

    Template Literal Type

---
+++ src/components/templateliterals/Toast.tsx
@@ -0,0 +1,20 @@
+/**
+ * Position can be one of
+ * "left-center" | "left-top" | "left-bottom" |
+ * "right-center" | "right-top" | "right-bottom" |
+ * "center-center" | "center-top" | "center-bottom" |
+ *
+ */
+
+type HorizontalPosition = "left" | "center" | "right";
+type VerticalPosition = "top" | "center" | "bottom";
+
+type ToastProps = {
+  position:
+    | Exclude<`${HorizontalPosition}-${VerticalPosition}`, "center-center">
+    | "center";
+};
+
+export const Toast = ({ position }: ToastProps) => {
+  return <div>Toast Notification - {position}</div>;
+};
