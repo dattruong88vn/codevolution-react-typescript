a434b304ae1ec2a698a0b61e446681ba640c435f
dattruong88vn <dattt1.ho@mcredit.com.vn>
2023-10-22 23:21:24 +0700

    Class Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Counter from "./components/class/Counter";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Counter message="Thanh Dat" />
+    </div>
+  );
 }
 
 export default App;
